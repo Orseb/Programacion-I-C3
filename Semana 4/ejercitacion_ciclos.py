d976b2e4074850840ad1d64467ecac10782411a2
Sebastián Ortiz <89535328+Orseb@users.noreply.github.com>
2023-08-29 16:30:42 -0300

    Update ejercitacion_ciclos.py

--- Semana 4/ejercitacion_ciclos.py
+++ Semana 4/ejercitacion_ciclos.py
@@ -1,3 +1,5 @@
+# EJERCICIO 1
+
 abecedario = 'abcdefghijklmnñopqrstuvwxyz'
 
 corrimiento = int(input("Corrimiento: "))
@@ -10,3 +12,29 @@ for x in range(5):
         if letra in abecedario:
             palabra += abecedario[(abecedario.index(letra)+corrimiento)%27]
     print(palabra)
+
+
+# EJERCICIO 2
+
+continuar = True
+total_pares = 0
+total_impares = 0
+
+while continuar:
+    numero = int(input("Ingrese un numero entero positivo: "))
+    par = 0
+    impar = 0
+    if numero != 0 and numero >0:
+        for n in str(numero):
+            if int(n)%2==0:
+                par += 1
+                total_pares += 1
+            else:
+                impar += 1
+                total_impares += 1
+        print(f"La cantidad de digitos impares son: {impar}")
+        print(f"La cantidad de digitos par son: {par}")
+    else:
+        print(f"La cantidad de digitos totales impares son: {total_impares}")
+        print(f"La cantidad de digitos totales par son: {total_pares}")
+        continuar = False
